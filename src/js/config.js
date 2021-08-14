193f7db622cb96e7b9928148a8bfcac81bf4f972
bonjourusman <bonjourusman@hotmail.com>
2021-08-14 17:59:00 +0400

    Helps and Config

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+// In this file (module), we will put all the variables that should be constants and should be reused across the project.
+
+// The goal of having this file with all these variables is that it will allow us to easily configure our project by simply changing some of the data included in this configuration file.
+
+// We will NOT want to put ALL the variables here; only those that are needed to define some important data about the application. E.g. the API url that goes into the fetch() function as it will be reused in multiple places across the project. Also, it might change in the future, so it would be useful to include it here.
+
+// Naming Convention: Uppercase denotes a constant that will never change
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 5;
